8529477b0ca5346a39180f37bfd144324d7c3592
Blackxes <blackxes.dev@gmail.com>
2020-09-20 20:48:36 +0200

    website title when app title not given

--- git_comsh.bat
+++ git_comsh.bat
@@ -3,7 +3,7 @@ set /p "force=Force push?: (y/n == anything else)"
 git add .
 git commit -m "%message%"
 
-if %force% equ y git push -f origin master
-if %force% neq y git push origin master
+if %force% equ "y" git push -f origin master
+if %force% neq "y" git push origin master
 
 pause
